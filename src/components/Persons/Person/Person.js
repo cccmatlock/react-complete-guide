b381bae5bdd15c27a1ba5c6d83dd1f63f911691e
Cameron Matlock <cameron.matlock@ruralsource.com>
2021-02-19 15:32:35 -0600

    getting deeper into react components

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,40 @@
+import React from 'react';
+//import styled from 'styled-components';
+import classes from './Person.module.css'
+
+
+// const StyledDiv = styled.div`        // styled components
+//     width: 60 %;
+//     margin: 16px auto;
+//     border: 1px solid #eee;
+//     box - shadow: 0 2px 3px #ccc;
+//     padding: 16px;
+//     text-align: center;
+
+
+//     @media(min-width: 500px) {
+//         width: 450px;
+//     }
+// `;
+
+const person = (props) => {
+    console.log('[Person.js] rendering...')
+    // const rndm = Math.random();
+
+    // if (rndm > 0.7) {
+    //     throw new Error('Something went wrong');
+    // }
+
+    return (
+        //<div className="Person" style={style}>
+        <div className={classes.Person}>
+
+            < p onClick={props.click} > I'm {props.name} and I am {props.age} years old!</p>
+            < p > {props.children}</p >
+            <input type="text" onChange={props.changed} value={props.name} />
+
+        </div>
+    )
+};
+
+export default person;
